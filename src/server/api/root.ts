66a5884aee90a3b1786bf077fe60a6826e7b4f79
Thomas Wilson <kilted.dev@gmail.com>
2023-04-17 16:36:37 -0400

    fix: cleanup stale files

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,8 +1,5 @@
-import { archetypeRouter } from '@malleus/server/api/routers/archetype';
 import { characterRouter } from '@malleus/server/api/routers/character';
 import { exampleRouter } from '@malleus/server/api/routers/example';
-import { talentRouter } from '@malleus/server/api/routers/talent';
-import { trainingRouter } from '@malleus/server/api/routers/training';
 import { createTRPCRouter } from '@malleus/server/api/trpc';
 
 /**
@@ -11,11 +8,8 @@ import { createTRPCRouter } from '@malleus/server/api/trpc';
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  archetype: archetypeRouter,
   character: characterRouter,
   example: exampleRouter,
-  training: trainingRouter,
-  talent: talentRouter,
 });
 
 // export type definition of API
